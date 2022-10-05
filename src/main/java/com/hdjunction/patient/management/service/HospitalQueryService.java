53b8e10be6cb405e3539fd2a7aa265d2fe064f39
daivd <xodnjs2549@naver.com>
2022-10-05 15:33:51 +0900

    refactor: 병원정보 조회로직을 query service 로 추출

---
+++ src/main/java/com/hdjunction/patient/management/service/HospitalQueryService.java
@@ -0,0 +1,23 @@
+package com.hdjunction.patient.management.service;
+
+import com.hdjunction.patient.management.domain.Hospital;
+import com.hdjunction.patient.management.repository.HospitalRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+
+@Service
+@Transactional(readOnly = true)
+public class HospitalQueryService {
+
+    private final HospitalRepository hospitalRepository;
+
+    public HospitalQueryService(HospitalRepository hospitalRepository) {
+        this.hospitalRepository = hospitalRepository;
+    }
+
+    public Hospital findHospital(Long id) {
+        return hospitalRepository.findById(id).orElseThrow(() -> new NoSuchElementException("존재하지 않는 병원입니다."));
+    }
+}
