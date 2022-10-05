573f98bdd42c7e1b8492f0d5610436daf81ce2b1
daivd <xodnjs2549@naver.com>
2022-10-05 15:32:26 +0900

    refactor: PatientService 를 query service, command service 로 분리

---
+++ src/main/java/com/hdjunction/patient/management/service/PatientCommandService.java
@@ -0,0 +1,53 @@
+package com.hdjunction.patient.management.service;
+
+import com.hdjunction.patient.management.domain.Hospital;
+import com.hdjunction.patient.management.domain.Patient;
+import com.hdjunction.patient.management.repository.PatientRepository;
+import com.hdjunction.patient.management.service.dto.RegisteringPatientFormRequest;
+import com.hdjunction.patient.management.service.dto.UpdatingPatientFormRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+
+@Service
+@Transactional
+public class PatientCommandService {
+
+    private final HospitalQueryService hospitalQueryService;
+    private final PatientQueryService patientQueryService;
+    private final PatientRepository patientRepository;
+
+    public PatientCommandService(HospitalQueryService hospitalQueryService, PatientQueryService patientQueryService, PatientRepository patientRepository) {
+        this.hospitalQueryService = hospitalQueryService;
+        this.patientQueryService = patientQueryService;
+        this.patientRepository = patientRepository;
+    }
+
+    /**
+     * 환자정보를 등록한다
+     */
+    public Long registerPatient(RegisteringPatientFormRequest request) {
+        Hospital hospital = hospitalQueryService.findHospital(request.getHospitalId());
+        Patient newPatient = patientRepository.save(request.toPatient(hospital));
+        return newPatient.getId();
+    }
+
+    /**
+     * 환자정보를 수정한다
+     */
+    public void updatePatient(Long id, UpdatingPatientFormRequest request) {
+        Long hospitalId = request.getHospitalId();
+        Hospital hospital = null;
+        if (Objects.nonNull(hospitalId)) {
+            hospital = hospitalQueryService.findHospital(hospitalId);
+        }
+
+        Patient patient = patientQueryService.findPatient(id);
+        patient.update(request.toPatient(hospital));
+    }
+
+    public void deletePatient(Long id) {
+        patientRepository.deleteById(id);
+    }
+}
--- src/main/java/com/hdjunction/patient/management/service/PatientQueryService.java
+++ src/main/java/com/hdjunction/patient/management/service/PatientQueryService.java
@@ -2,7 +2,6 @@ package com.hdjunction.patient.management.service;
 
 import com.hdjunction.patient.management.domain.Patient;
 import com.hdjunction.patient.management.repository.PatientRepository;
-import com.hdjunction.patient.management.service.dto.FindingPatientResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,16 +21,6 @@ public class PatientQueryService {
      * 환자정보를 조회한다
      */
     public Patient findPatient(Long id) {
-        Patient patient = patientRepository.findById(id).orElseThrow(() -> new NoSuchElementException("존재하지 않는 환자정보입니다."));
-        return patient;
-    }
-
-    /**
-     * 환자정보와 환자의 모든 내원정보를 조회한다
-     */
-    // todo assembler 패턴 적용
-    public FindingPatientResponse findPatientAndVisit(Long id) {
-        Patient patient = findPatient(id);
-        return new FindingPatientResponse(patient, null);
+        return patientRepository.findById(id).orElseThrow(() -> new NoSuchElementException("존재하지 않는 환자정보입니다."));
     }
 }
