984a96ab624081133d1e60f80ea003a3d42a1950
daivd <xodnjs2549@naver.com>
2022-10-05 10:52:45 +0900

    feat: 환자등록번호 채번용 시퀀스 쿼리 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,3 @@
+/* 환자등록번호 채번용 시퀀스 */
+drop sequence if exists patient_registration_number_sequence;
+create sequence patient_registration_number_sequence as bigint minvalue 1 maxvalue 9999999999999 increment by 1;
